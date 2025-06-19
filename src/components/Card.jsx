6446163b8a1bfd5c3e05f437c4faac71fccba0ec
Alex82-estrada <alexestrada2907@gmail.com>
2025-06-19 15:37:54 -0700

    implementando el componente Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+function Card({name,email}){
+    console.log(name)
+    console.log(email)
+    return(
+        <div className="column is-one-quarter">
+            <div className="card">
+                <div className="card-imagen">
+                    <figure className="image is-4by3">
+                        <img 
+                         src={`https://robohash.org/${name}`}
+                         alt={`${email}`}
+                        />
+                    </figure>
+                </div>
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-content">
+                            <p className="title is-4">\{name}</p>
+                            <p className="sustitle is-5">{email}</p>
+                        
+                    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Card
