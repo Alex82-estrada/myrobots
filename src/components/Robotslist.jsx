26e99c70743247774843be85a8c56ad54a65fa4d
Alex82-estrada <alexestrada2907@gmail.com>
2025-06-19 15:23:56 -0700

    agrege el componente Robotslist

---
+++ src/components/Robotslist.jsx
@@ -0,0 +1,20 @@
+import Card from "./Card"
+
+function Robotslist({ robots }){
+    const renderRobot = robots.map((robots)=>{
+
+        return <Card key={robot.id} name={robot.name} email={robot.email} />
+    })
+
+    return(
+        <div className="container">
+            <div className="columns is-multiline">
+                {renderRobot}
+            </div>
+        </div>
+    )
+
+}
+
+
+export default Robotslist
